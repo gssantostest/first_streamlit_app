b92d07a4ac9a54b5d9a656a79f83af6f44406f6c
gssantostest <135095225+gssantostest@users.noreply.github.com>
2023-05-31 16:30:10 +0800

    Update streamlit_app.py
    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruitchoice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 # FORMATTING THE JSON - write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
